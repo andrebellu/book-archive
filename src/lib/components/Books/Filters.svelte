533fe2536c620af340caee645e4b66830dc72524
andrebellu <bellu.andrea@outlook.it>
2024-10-16 11:37:18 +0200

    added filters

---
+++ src/lib/components/Books/Filters.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { books, filteredBooks } from "../../../store.js";
+
+    function handleFilter(e) {
+        let selectedBooks = [];
+        let event = e.target.className;
+
+        if (event === "material-symbols-outlined") {
+            event = e.target.parentElement.className;
+        }
+
+        switch (event) {
+            case "wishlist filter":
+                selectedBooks = $books.filter(
+                    (book) => book.status === "wishlist"
+                );
+                break;
+            case "library filter":
+                selectedBooks = $books.filter((book) => book.status === "lib");
+                break;
+            case "read filter":
+                selectedBooks = $books.filter((book) => book.status === "read");
+                break;
+            case "reading filter":
+                selectedBooks = $books.filter(
+                    (book) => book.status === "reading"
+                );
+                break;
+            default:
+                filteredBooks.update((b) => $books);
+        }
+
+        filteredBooks.update((b) => selectedBooks);
+    }
+</script>
+
+<div class="join">
+    <div class="wishlist filter" on:click={handleFilter}>
+        <span class="material-symbols-outlined"> bookmarks </span>
+    </div>
+
+    <div class="library filter" on:click={handleFilter}>
+        <span class="material-symbols-outlined"> library_books </span>
+    </div>
+
+    <div class="read filter" on:click={handleFilter}>
+        <span class="material-symbols-outlined"> done_all </span>
+    </div>
+
+    <div class="reading filter" on:click={handleFilter}>
+        <span class="material-symbols-outlined"> menu_book </span>
+    </div>
+</div>
--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,63 +1,68 @@
 <script>
-  import { onMount } from "svelte";
-  import BookCard from "$lib/components/Books/BookCard.svelte";
-  import Add from "$lib/components/Books/Add.svelte";
-  import { books, authors, filteredBooks } from "../../store.js";
-  import Search from "../../lib/components/Search.svelte";
-  import BookCardList from "$lib/components/Books/BookCardList.svelte";
-  import AddMore from "../../lib/components/Books/AddMore.svelte";
-  export let data;
+    import { onMount } from "svelte";
+    import BookCard from "$lib/components/Books/BookCard.svelte";
+    import Add from "$lib/components/Books/Add.svelte";
+    import { books, authors, filteredBooks } from "../../store.js";
+    import Search from "$lib/components/Search.svelte";
+    import BookCardList from "$lib/components/Books/BookCardList.svelte";
+    import AddMore from "$lib/components/Books/AddMore.svelte";
+    import Filters from "$lib/components/Books/Filters.svelte";
+    export let data;
 
-  let list = false;
+    let list = false;
 
-  onMount(() => {
-    setVariables();
-  });
-
-  function setVariables() {
-    books.set(data.books);
-
-    filteredBooks.set(data.books);
-
-    books.update((books) => {
-      return books.sort((a, b) => {
-        if (!a.read && b.read) return -1;
-        if (a.reading && !b.reading) return -1;
-        return 0;
-      });
+    onMount(() => {
+        setVariables();
     });
 
-    authors.set(data.authors);
-  }
+    function setVariables() {
+        books.set(data.books);
 
-  async function handleList() {
-    list = !list;
-  }
+        filteredBooks.set(data.books);
 
-  $: $books = $books;
+        books.update((books) => {
+            return books.sort((a, b) => {
+                if (!a.read && b.read) return -1;
+                if (a.reading && !b.reading) return -1;
+                return 0;
+            });
+        });
+
+        authors.set(data.authors);
+    }
+
+    async function handleList() {
+        list = !list;
+    }
+
+    $: $books = $books;
 </script>
 
 <body class="flex flex-col items-center no-scroll">
-  <div class="flex flex-row-reverse items-center gap-4 p-10">
-    <Search />
-    <button class="btn btn-gray hover:bg w-32" on:click={handleList}>
-      {list ? "Grid" : "List"}
-    </button>
-    <!--<AddMore />-->
-    <Add />
-  </div>
+    <div class="flex flex-row-reverse items-center gap-4 p-10">
+        <Search />
+        <button class="btn btn-gray hover:bg w-32" on:click={handleList}>
+            {list ? "Grid" : "List"}
+        </button>
+        <Filters />
+        <Add />
+    </div>
 
-  {#if list}
-    <div class="grid grid-cols-4 px-10 gap-8">
-      {#each $books as book}
-        <BookCardList {book} />
-      {/each}
-    </div>
-  {:else}
-    <div class="flex justify-center items-center flex-wrap gap-10 px-10">
-      {#each $filteredBooks as book}
-        <BookCard {book} />
-      {/each}
-    </div>
-  {/if}
+    {#if list}
+        <div class="grid grid-cols-4 px-10 gap-8">
+            {#each $books as book}
+                <BookCardList {book} />
+            {/each}
+        </div>
+    {:else}
+        <div class="flex justify-center items-center flex-wrap gap-10 px-10">
+            {#each $filteredBooks as book}
+                <BookCard {book} />
+            {/each}
+        </div>
+    {/if}
+
+    {#if $filteredBooks.length === 0}
+        <h1>No books found!</h1>
+    {/if}
 </body>
